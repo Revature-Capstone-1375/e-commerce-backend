5b4be2f082ad70a1dc636267721da83e8c85313f
Matthew Coplan <mcoplanwork@gmail.com>
2022-07-26 18:46:28 -0400

    Created UserController and added deleteUser endpoint

--- src/main/java/com/revature/controllers/UserController.java
+++ src/main/java/com/revature/controllers/UserController.java
@@ -1,2 +1,22 @@
-package com.revature.controllers;public class UserController {
+package com.revature.controllers;
+
+import com.revature.models.User;
+import com.revature.services.UserService;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    public void deleteUser(@RequestBody User user) {
+        userService.deleteUser(user);
+    }
 }
