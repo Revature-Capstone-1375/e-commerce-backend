316bc33a5882de8a44171ec00ccefda49135fb5f
Matthew Coplan <mcoplanwork@gmail.com>
2022-07-26 14:43:02 -0400

    Created enums for UserRoles and added to User Model

--- src/main/java/com/revature/models/User.java
+++ src/main/java/com/revature/models/User.java
@@ -1,5 +1,6 @@
 package com.revature.models;
 
+import com.revature.models.enums.UserRoles;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -20,4 +21,5 @@ public class User {
     private String password;
     private String firstName;
     private String lastName;
+    private UserRoles role;
 }
